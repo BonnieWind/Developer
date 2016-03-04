2a3526b9f946390fe1cd529299a6b9236aabe15b
YangHuiling <iamyanghl@163.com>
2016-03-04 17:50:38 +0800

    [task]add Tom.java.

---
+++ Tom.java
@@ -0,0 +1,19 @@
+public class Tom{
+         int leg;
+	String head;
+	void cry(String s){
+	System.out.println(s);
+	}
+}
+
+class temp{
+public static void main(String[] args){
+        Tom cat;
+	cat=new Tom();
+	cat.leg:=4;
+	cat.head:="cat";
+	System.out.println("the number of cat's leg:"+cat.leg);
+	System.out.println("the name of cat's head:"+cat.head);
+	cat.cry(Jerry, kill u!);
+	}
+}
